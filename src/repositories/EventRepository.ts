0ecd3bb6666d74ebd29b4211c4ca55edf55c53a5
Vyctor <vkvyctor180@gmail.com>
2025-05-27 08:14:50 -0300

    Adicioando apenas a documentação na branch tr_docs para poder usar no github pages

--- src/repositories/EventRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Event } from "../models/Event";
-import { AppDataSource } from "../db_config/AppDataSource";
-
-/*
- * Repositório TypeORM para a entidade 'Event'.
- * Fornece métodos para interagir com a tabela de eventos no banco de dados.
- */
-export const eventRepository = AppDataSource.getRepository(Event);
--- src/repositories/PlaceRepository.ts
+++
@@ -1,8 +0,0 @@
-import { Place } from "../models/Place";
-import { AppDataSource } from "../db_config/AppDataSource";
-
-/*
- * Repositório TypeORM para a entidade 'Place'.
- * Fornece métodos para interagir com a tabela de lugares no banco de dados.
- */
-export const placeRepository = AppDataSource.getRepository(Place);

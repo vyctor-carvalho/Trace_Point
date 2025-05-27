6d2d29ea5c44cd3a8e41b78a2cdbcb3343c0fc4a
Vyctor <vkvyctor180@gmail.com>
2025-05-27 07:59:40 -0300

    Retirando a Documentação da branch principal

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import express from "express"
-import server from "./sever";
+import server from './sever';
 import { PORT } from "./config/EsportEnv";
 import { errorsHandler } from "./middleware/ErrorsHandler";
 import { authRoutes } from "./routes/AuthRoutes";
@@ -7,7 +6,6 @@ import { userRouter } from "./routes/UserRoutes";
 import { eventRouter } from "./routes/EventRoutes";
 import { placeRouter } from "./routes/PlaceRoutes";
 import { visitedRoutes } from "./routes/VisitedRoutes";
-import path from 'path';
 
 /**
  * @file index.ts
@@ -18,22 +16,9 @@ import path from 'path';
 
 const port = PORT
 
-
-server.use(express.static(path.join(process.cwd(), 'public')));
-
 // Rota raiz para verificação de funcionamento
 server.get('/', (req, res) => {
-  // Constrói o caminho absoluto para o arquivo HTML
-  // process.cwd() retorna o diretório de trabalho atual do processo Node.js
-  // Assumindo que a pasta 'public' está na raiz do seu projeto
-  const filePath = path.join(process.cwd(), 'public', 'index.html');
-  res.sendFile(filePath, (err) => {
-    if (err) {
-      console.error("Erro ao enviar o arquivo HTML:", err);
-      // Envie uma resposta de erro genérica ou trate o erro de forma mais específica
-      res.status((err as any).status || 500).send("Erro ao carregar a página.");
-    }
-  });
+  res.send('<h1>Trace Point API</h1><p>Bem-vindo à API do Trace Point!</p>');
 });
 
 // Configuração das rotas da aplicação
